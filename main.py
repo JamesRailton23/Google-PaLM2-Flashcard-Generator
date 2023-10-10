8383a51aacf16cecd2bcd8cda945821650b536c3
James Railton <jamesrailton2003@gmail.com>
2023-10-10 16:09:09 +0100

    Update 1.0

--- main.py
+++ main.py
@@ -3,19 +3,26 @@ import sys
 import google.generativeai as palm
 #api key
 palm.configure(api_key="here")
-model = palm.get_model("models/text-bison")
-prompt = ("Create a set of flashcards in question-and-answer format from the provided notes, using simple and "
-          "easy-to-remember language, and formatting the flashcards into a table with two columns: one for the "
-          "question and one for the answer.")
+aimodel = palm.get_model("models/text-bison")
+prompt = ("Create a set of flashcards in question-and-answer format from the provided notes below the colon, "
+          "using simple and easy-to-remember language, and formatting the flashcards into a table with two columns "
+          "one for the"
+          " question and one for the answer: ")
 
 def generateflashcard():
-    create_flashcards = int(input("Generate flashcards?: 1 for yes and 0 for exit: "))
-    if create_flashcards:
-        with open("input.txt") as input_file:
-            file = input_file.read()
-            print(file)
+    with open("input.txt") as input_file:
+        file = input_file.read()
+        flashcards = str(palm.generate_text(model=aimodel, prompt=prompt+file))
+    with open("ouput.txt","w") as output_file:
+        output_file.write(flashcards)
+        output_file.read()
+    if int(input("Re-generate?: 1 for yes or 0 for no: ")):
+        generateflashcard()
     else:
         sys.exit()
 
 
-generateflashcard()
+if int(input("Generate flashcards?: 1 for yes and 0 for exit: ")):
+    generateflashcard()
+else:
+    sys.exit()
